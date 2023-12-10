47ea82f75f852a8bb42ec54f7c4df86d2fa970ba
Kacper <andziakwozniak@gmail.com>
2023-12-10 14:47:42 +0100

    webhook with test creds

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -42,10 +42,6 @@ export class PaymentsService {
 
   async handleWebhookRequest(body: any, signature: any): Promise<any> {
     const secretEndpoint = this.configService.get('STRIPE_WEBHOOK_TEST');
-    console.log(secretEndpoint);
-    console.log(body.toString());
-    console.log(signature);
-
     const event = this.stripe.webhooks.constructEvent(
       body,
       signature,
